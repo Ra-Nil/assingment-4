f9104e5a962ce15a25aab43da27d6b67662e95e5
ranilenail <ranilchaudhary2025@mic.edu.np>
2026-06-19 19:27:06 +0545

    git commit -m "Add Ranil.java file"

---
+++ Ranil.java
@@ -0,0 +1,11 @@
+public class Ranil{
+    public static void main(String[] args) {
+        System.out.println("Hello, Ranil!");
+
+        int x = 5;
+        int y = 15;
+        int product = x * y;
+
+        System.out.println("Product = " + product);
+    }
+}
